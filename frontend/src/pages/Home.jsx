6c1df817a9a0b9d36f9fbd40beb34af893276496
shivang <shivang28chauhan@gmail.com>
2024-07-28 04:46:45 +0530

    Category Section Created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Categories from '../components/Categories'
 import Hero from '../components/Hero'
+
 const Home = () => {
+
+  const [category,setCategory]=useState('All')
   return (
     <>
       <Hero />
-      <Categories/>
+      <Categories category={category} setCategory={setCategory}/>
     </>
   )
 }
