6642ddd414bc53992e21aa5eaf1dbec0a6157e3e
shivang <shivang28chauhan@gmail.com>
2024-08-11 01:07:22 +0530

    Admin Page Modified and Functionality Added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,27 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
+import Add from "./pages/Add"
+import List from "./pages/List"
+import Orders from "./pages/Orders"
+
 
 export default function App() {
   return (
    <BrowserRouter>
-
+   <ToastContainer/>
+    <Navbar/>
+    <hr />
+    <div className="flex">
+      <Sidebar/>
+      <Routes>
+        <Route path="/add" element={<Add />}/>
+        <Route path="/list" element={<List />}/>
+        <Route path="/orders" element={<Orders />}/>
+      </Routes>
+    </div>
    </BrowserRouter>
   )
 }
